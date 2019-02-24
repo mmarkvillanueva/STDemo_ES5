ffd18907d0e93dc706162322d2b33b8ce8d00dce
mmarkvillanueva <mmarkvillanueva@gmail.com>
2019-02-24 19:50:11 +0800

    Initial Version

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("com.st.demostdemo_es5.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("com.st.demostdemo_es5.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
